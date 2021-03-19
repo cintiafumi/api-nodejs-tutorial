32c424147cfd30822105fab031528a55e1d47dbe
Cíntia Fumi <cintiafumi@gmail.com>
2021-03-19 15:25:16 -0300

    ✨  feat(route): add basic GET route

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send({ app: 'Hello' });
+});
+
+app.listen(PORT);
