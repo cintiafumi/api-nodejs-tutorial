3295cba5cfaeb39533b0bb43e88d1828984335b3
Cíntia Fumi <cintiafumi@gmail.com>
2021-03-21 16:10:46 -0300

    🥅  chore: handle error

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(routes);
+
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV === 'production')
+    res.status(500).json({ error: 'Internal server error.' });
+  else return next(err);
+});
 
 app.listen(PORT);
